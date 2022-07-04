e09c3c3f60c11cb67a7297ef20a443fa86ceeb0b
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-04 22:00:38 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -27,80 +27,21 @@ from plugins.functions.ran_text import random_char
 from plugins.database.add import add_user_to_database
 from pyrogram.types import Thumbnail
 
-@Client.on_message(filters.private & filters.regex(pattern=".*http.*"))
-async def echo(bot, update):
-    if Config.LOG_CHANNEL:
-        try:
-            log_message = await update.forward(Config.LOG_CHANNEL)
-            log_info = "Message Sender Information\n"
-            log_info += "\nFirst Name: " + update.from_user.first_name
-            log_info += "\nUser ID: " + str(update.from_user.id)
-            log_info += "\nUsername: @" + update.from_user.username if update.from_user.username else ""
-            log_info += "\nUser Link: " + update.from_user.mention
-            await log_message.reply_text(
-                text=log_info,
-                disable_web_page_preview=True,
-                quote=True
-            )
-        except Exception as error:
-            print(error)
-    if not update.from_user:
-        return await update.reply_text("😬 Something went wrong with your profile at telegram or Pyrogram side.")
-    await add_user_to_database(bot, update)
+@Client.on_message(filters.regex(pattern=".*http.*"))
+async def echo(bot, update: Message):
 
-    if Config.UPDATES_CHANNEL:
-      fsub = await handle_force_subscribe(bot, update)
-      if fsub == 400:
-        return
+    # LOGGER.info(update)
+    # await bot.send_chat_action(
+    #     chat_id=update.chat.id,
+    #     action="typing"
+    # )
     logger.info(update.from_user)
-    url = update.text
-    youtube_dl_username = None
-    youtube_dl_password = None
-    file_name = None
-
-    print(url)
-    if "|" in url:
-        url_parts = url.split("|")
-        if len(url_parts) == 2:
-            url = url_parts[0]
-            file_name = url_parts[1]
-        elif len(url_parts) == 4:
-            url = url_parts[0]
-            file_name = url_parts[1]
-            youtube_dl_username = url_parts[2]
-            youtube_dl_password = url_parts[3]
-        else:
-            for entity in update.entities:
-                if entity.type == "text_link":
-                    url = entity.url
-                elif entity.type == "url":
-                    o = entity.offset
-                    l = entity.length
-                    url = url[o:o + l]
-        if url is not None:
-            url = url.strip()
-        if file_name is not None:
-            file_name = file_name.strip()
-        # https://stackoverflow.com/a/761825/4723940
-        if youtube_dl_username is not None:
-            youtube_dl_username = youtube_dl_username.strip()
-        if youtube_dl_password is not None:
-            youtube_dl_password = youtube_dl_password.strip()
-        logger.info(url)
-        logger.info(file_name)
-    else:
-        for entity in update.entities:
-            if entity.type == "text_link":
-                url = entity.url
-            elif entity.type == "url":
-                o = entity.offset
-                l = entity.length
-                url = url[o:o + l]
-    if Config.HTTP_PROXY != "":
+    url, _, youtube_dl_username, youtube_dl_password = get_link(update)
+    if Config.HTTP_PROXY is not None:
         command_to_exec = [
             "yt-dlp",
             "--no-warnings",
-            "--youtube-skip-hls-manifest",
+            "--youtube-skip-dash-manifest",
             "-j",
             url,
             "--proxy", Config.HTTP_PROXY
@@ -109,7 +50,7 @@ async def echo(bot, update):
         command_to_exec = [
             "yt-dlp",
             "--no-warnings",
-            "--youtube-skip-hls-manifest",
+            "--youtube-skip-dash-manifest",
             "-j",
             url
         ]
@@ -119,38 +60,20 @@ async def echo(bot, update):
     if youtube_dl_password is not None:
         command_to_exec.append("--password")
         command_to_exec.append(youtube_dl_password)
-    logger.info(command_to_exec)
-    chk = await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.CHECK_LINK,
-            disable_web_page_preview=True,
-            reply_to_message_id=update.id,
-            parse_mode=enums.ParseMode.HTML
-          )
-    process = await asyncio.create_subprocess_exec(
-        *command_to_exec,
-        # stdout must a pipe to be accessible as process.stdout
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    # Wait for the subprocess to finish
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    logger.info(e_response)
-    t_response = stdout.decode().strip()
-    #logger.info(t_response)
+    # logger.info(command_to_exec)
+    t_response, e_response = await run_shell_command(command_to_exec)
     # https://github.com/rg3/youtube-dl/issues/2630#issuecomment-38635239
     if e_response and "nonnumeric port" not in e_response:
         # logger.warn("Status : FAIL", exc.returncode, exc.output)
-        error_message = e_response.replace("Unsupported UrI !", "")
-        if "This video is only available for registered users." in error_message:
+        error_message = e_response.replace(
+            Translation.YTDL_ERROR_MESSAGE,
+            ""
+        )
+        if Translation.ISOAYD_PREMIUM_VIDEOS in error_message:
             error_message += Translation.SET_CUSTOM_USERNAME_PASSWORD
-        await chk.delete()
-        time.sleep(1)
-        await bot.send_message(
-            chat_id=update.chat.id,
+        await update.reply_text(
             text=Translation.NO_VOID_FORMAT_FOUND.format(str(error_message)),
-            reply_to_message_id=update.id,
+            quote=True,
             parse_mode=enums.ParseMode.HTML,
             disable_web_page_preview=True
         )
@@ -161,15 +84,15 @@ async def echo(bot, update):
         if "\n" in x_reponse:
             x_reponse, _ = x_reponse.split("\n")
         response_json = json.loads(x_reponse)
-        randem = random_char(5)
         save_ytdl_json_path = Config.DOWNLOAD_LOCATION + \
-            "/" + str(update.from_user.id) + f'{randem}' + ".json"
+            "/" + str(update.from_user.id) + ".json"
         with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
             json.dump(response_json, outfile, ensure_ascii=False)
         # logger.info(response_json)
-        #inline_keyboard = []
-
         inline_keyboard = []
+
+
+        #inline_keyboard = []
         for formats in response_json["formats"]:
             format_id = formats["format_id"]
             format_string = formats["format"]
