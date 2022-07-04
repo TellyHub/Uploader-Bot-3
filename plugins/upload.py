079dd3b9d746aaecfe6afeb7b1d2365677724d37
TellyHub <106040852+TellyHub@users.noreply.github.com>
2022-07-04 15:08:34 +0530

    Update upload.py

--- plugins/upload.py
+++ plugins/upload.py
@@ -1,4 +1,48 @@
+#!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+#  Copyright (C) 2020 PublicLeech Authors
 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# the logging things
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
+import asyncio
+import os
+import time
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from PIL import Image
+from pyrogram.types import (
+    InputMediaDocument,
+    InputMediaVideo,
+    InputMediaAudio
+)
+from plugins.functions.display_progress import (
+    progress_for_pyrogram,
+    humanbytes
+)
+from functions.help_Nekmo_ffmpeg import *
+from plugins.split_large_file import split_large_files
+from plugins.functions.copy_similar_file import copy_file
+from plugins.config import Config 
 
 
 
